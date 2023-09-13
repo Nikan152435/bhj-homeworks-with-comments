6f52a074a6da434702273a54d763304df262fbc6
NikaN <NikaN7602@gmail.com>
2023-09-13 20:55:03 +0200

    README.md

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,11 +1,16 @@
-//Домашнее задание к лекции «Возможности JavaScript в браузере»
-// Задача 2  Игра кликер
+// Задача 2   Игра кликер
 
-let cookieID = document.getElementById ('cookie');
-let counterID = document.getElementById('clicker__counter');
+let cookieID = document.getElementById ('cookie');// getElementById Осущесвляет посик элемента по его идентификатору
+let counterID = document.getElementById('clicker__counter'); //Элемент находится в глобальной области видимости
 let counter = 0;
 
-cookieID.addEventListener('click', function() {
-  counterID.textContent = ++counter;
-  cookieID.width = counter % 2 == 0 ? 200 : 250;
+cookieID.addEventListener('click', function() { // встроенная функция в JavaScript, которая принимает событие для прослушивания и второй аргумент, 
+  //который вызывается всякий раз, когда описанное событие запускается
+  counterID.textContent = ++counter; //про ++ - схожее с предыдущим пунктом/объяснением?
+  // textContent это свойство DOM, которое используется для установки текстового содержимого для элемента HTML или получения текстового содержимого
+  cookieID.width = counter % 2 == 0 ? 200 : 250;//cookieID.width = counter % 2 == 0 ? 200 : 250; - это тернарный оператор,
+  // т.е. если остаток от деления текущего значения переменной counter равен ноль, то устанавливаем ширину (картинки/печеньки) в 200рх, 
+  //иначе - в 250рх.
 });
+
+//Коменнты преподаввателя
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,18 +1,27 @@
-//Домашнее задание к лекции «Возможности JavaScript в браузере»
-// Задача Убей кротов
-'use strict'
+//Задача 3   Убей кротов
+
+'use strict' // Директива use strict включает строгий режим JavaScript . Строгий режим JavaScript был введен в ECMAScript 5. 
+//Он обеспечивает более строгий анализ и обработку ошибок кода во время выполнения. Это также помогает вам писать более чистый код и
+// выявлять ошибки и ошибки, которые в противном случае могли бы остаться незамеченными
+
 let getHole = index => document.getElementById(`hole${index}`);
+// getElementById Осущесвляет посик элемента по его идентификатору
 let deadMole = document.getElementById('dead');
 let lostMole = document.getElementById('lost');
-let wings = +deadMole.textContent;
+let wings = +deadMole.textContent; //// textContent это свойство DOM, которое используется для установки текстового содержимого для 
+//элемента HTML или получения текстового содержимого
 let losts = +lostMole.textContent;
 
 for (let i = 1; i < 10; i++) {
   getHole(i).onclick = function () {
-    if (this.className.includes('hole_has-mole')) {
-      deadMole.textContent = ++wings;
+// в getHole же сохранили стрелочную ф-ию, поэтому теперь это вызов ф-ии с передачей ей аргумента и 
+//на результат её работы (конкретное поле) навешан обработчик события клик. this это объект, у него есть свойство className, 
+//его значение - строка. includes - это метод строки.  
+    if (this.className.includes('hole_has-mole')) { 
+      // Метод includes() возвращает true, если строка содержит указанную строку. 
+      deadMole.textContent = ++wings; //Установка текстового содержимого
     } else {
-      lostMole.textContent = ++losts;
+      lostMole.textContent = ++losts;//Установка текстового содержимого
     };
 
     if (wings === 10) {
